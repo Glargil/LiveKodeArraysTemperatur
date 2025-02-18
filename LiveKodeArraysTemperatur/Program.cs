6bc4c64d2d9dc26cd89900555cffcc81696888c0
egilb <egilb@EGILTHINKPAD>
2025-02-18 12:43:02 +0100

    Add project files.

---
+++ LiveKodeArraysTemperatur/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace LiveKodeArraysTemperatur
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] dayTemperatur = new int [8] { 7, 4, 5, 5, 5, 4, 4, 4};
+
+            int dayTemperaturTotal = 0;
+
+            double maxTemperatur = dayTemperatur[0];
+
+            double minTemperatur = dayTemperatur[0];
+
+            for (int i = 0; i < dayTemperatur.Length; i++)
+            {
+                dayTemperaturTotal += dayTemperatur[i];
+
+                if (dayTemperatur[i] > maxTemperatur) //checker hele tiden om daytemp er højere end maxtemp hvis den er så ændre vi maxtemp til den nye værdi
+                {
+                    maxTemperatur = dayTemperatur[i];
+                }
+                if (dayTemperatur[i] < minTemperatur) //checker hele tiden om daytemp er højere end maxtemp hvis den er så ændre vi maxtemp til den nye værdi
+                {
+                    minTemperatur = dayTemperatur[i];
+                }
+            }
+            Console.WriteLine("Gennemsnits temperaturen er " + dayTemperaturTotal / dayTemperatur.Length);
+            Console.WriteLine("mindste temperaturen er " + minTemperatur + " og maks temperaturen er " + maxTemperatur);
+        }
+    }
+}
